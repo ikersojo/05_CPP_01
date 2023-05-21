e8477cdcdd830852f06ac610a699ca0d75629ee8
Iker Sojo <ikersojo@hotmail.com>
2023-05-21 16:29:39 +0200

    update

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/21 16:28:21 by isojo-go          #+#    #+#             */
+/*   Created: 2023/05/21 16:29:18 by isojo-go          #+#    #+#             */
 /*   Updated: 2023/05/21 16:29:26 by isojo-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-#define ZOMBIE_HPP
-#include <string>
-#include <iostream>
-
-class	Zombie
-{
-	public:
-		~Zombie(void);
-		Zombie(void);
-		Zombie(std::string name);
-
-		void			announce(void);
-		void			setName(std::string name);
-
-		static Zombie*	newZombie(std::string name);
-		static void		randomChump(std::string name);
-		static Zombie*	zombieHorde(int N, std::string name);
-
-	private:
-		std::string	_name;
-};
-
-#endif // ZOMBIE_HPP
